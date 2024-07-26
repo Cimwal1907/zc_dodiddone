1cfeb03cab6f16092cfaea4525a07d2f28cbcd29
Cimwal1907 <web.programmer39@gmail.com>
2024-07-26 20:23:56 +0300

    razmetka

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-
 import '../pages/my_home_pages.dart';
 import '../theme/theme.dart';
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: DoDidDoneThemeData.lightTheme, // Use the defined theme
+      theme: DoDidDoneTheme.lightTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-
-class DoDidDoneThemeData {
+class DoDidDoneTheme {
   static ThemeData lightTheme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
       seedColor: const Color(0xFF9F7BF6), // Primary color
       brightness: Brightness.light,
       primary: const Color(0xFF9F7BF6), // Primary color
-      secondary: const Color(0xFF4CEB8B), // Secondary color
-      // ... other color scheme properties
+      secondary: const Color(0xFF4CE8B8), // Secondary color
     ),
     useMaterial3: true,
   );
